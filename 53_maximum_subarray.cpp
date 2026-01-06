0687830c6c9a0015dff743f51916c94424d6af99
Eklas Mohin <mohin73k@gmail.com>
2026-01-06 21:42:27 +0600

    LeetCode 53. Maximum Subarray

---
+++ 53_maximum_subarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = nums[0], curr_sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            curr_sum += nums[i];
+            max_sum = max(max_sum, curr_sum);
+            if (curr_sum < 0) {
+                curr_sum = 0;
+            }
+        }
+        return max_sum;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    Solution solution;
+    int max_subarray_sum = solution.maxSubArray(nums);
+    cout << max_subarray_sum << '\n';
+    return 0;
+}
