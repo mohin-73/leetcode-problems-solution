02f3b90d2e1508670d4baf14e241382a0e810897
Eklas Mohin <mohin73k@gmail.com>
2026-01-06 23:15:53 +0600

    LeetCode 167. Two Sum II - Input Array Is Sorted

--- 167_two_sum_ii.cpp
+++ 167_two_sum_ii.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int j = numbers.size() - 1;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int j = nums.size() - 1;
         for (int i = 0; i < j; ++i) {
-            if (numbers[i] + numbers[j] == target) {
+            if (nums[i] + nums[j] == target) {
                 return {i + 1, j + 1};
-                i = j;
-                break;
-            } else if (numbers[i] + numbers[j] > target) {
+            } else if (nums[i] + nums[j] > target) {
                 --j;
                 --i;
             }
@@ -23,10 +21,10 @@ public:
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<int> numbers = {2, 7, 11, 15};
+    vector<int> nums = {2, 7, 11, 15};
     int target = 9;
     Solution solution;
-    vector<int> idx = solution.twoSum(numbers, target);
+    vector<int> idx = solution.twoSum(nums, target);
     cout << idx[0] << ' ' << idx[1] << '\n';
     return 0;
 }
