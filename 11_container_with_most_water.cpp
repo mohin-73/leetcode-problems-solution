95bafc259dbcdb1b593297e8d29cdc02cacc7266
Eklas Mohin <mohin73k@gmail.com>
2026-01-06 22:01:39 +0600

    LeetCode 11. Container With Most Water

--- 11_container_with_most_water.cpp
+++ 11_container_with_most_water.cpp
@@ -6,13 +6,13 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0, right = height.size() - 1;
-        int max_water = 0;
+        int maxWater = 0;
         while (right > left) {
             int water = (right - left) * min(height[left], height[right]);
-            max_water = max(max_water, water);
+            maxWater = max(maxWater, water);
             height[right] > height[left] ? ++left : --right;
         }
-        return max_water;
+        return maxWater;
     }
 };
 
