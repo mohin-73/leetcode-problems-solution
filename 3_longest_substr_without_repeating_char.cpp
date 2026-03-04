c458340cf978dad627aa746206bfc491024959fa
Eklas Mohin <mohin73k@gmail.com>
2026-03-04 18:37:59 +0600

    LeetCode 3. Longest Substring Without Repeating Characters

--- 3_longest_substr_without_repeating_char.cpp
+++ 3_longest_substr_without_repeating_char.cpp
@@ -5,20 +5,19 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        bool check[256];
-        memset(check, false, sizeof(check));
-        int left = 0, right = 0, maxLength = 0;
+        int left = 0, right = 0, maxLen = 0;
+        vector<bool> present(256, false);
         while (right < s.length()) {
-            if (!check[s[right]]) {
-                check[s[right]] = true;
+            if (!present[s[right]]) {
+                present[s[right]] = true;
                 ++right;
             } else {
-                check[s[left]] = false;
+                present[s[left]] = false;
                 ++left;
             }
-            maxLength = max(maxLength, right - left);
+            maxLen = max(maxLen, right - left);
         }
-        return maxLength;
+        return maxLen;
     }
 };
 
@@ -26,8 +25,47 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     string s = "l;gn*sln%gh";
-    Solution solution;
-    int maxLen = solution.lengthOfLongestSubstring(s);
-    cout << maxLen << '\n';
+    Solution solve;
+    int result = solve.lengthOfLongestSubstring(s);
+    cout << result << '\n';
     return 0;
 }
+
+/*
+    ------------------
+    Problem Statement:
+    ------------------
+    Given a string s, find the length of the longest substring without duplicate characters.
+
+    ----------
+    Example 1:
+    ----------
+
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+    ----------
+    Example 2:
+    ----------
+
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+    ----------
+    Example 3:
+    ----------
+
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+    
+    ------------
+    Constraints:
+    ------------
+
+    0 <= s.length <= 5 * 10^4
+    s consists of English letters, digits, symbols and spaces.
+*/
