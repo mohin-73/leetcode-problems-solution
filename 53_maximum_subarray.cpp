83a4eb87c00a2f17f49c4783371561e21aa1911c
Eklas Mohin <mohin73k@gmail.com>
2026-03-07 05:07:54 +0600

    LeetCode 53. Maximum Subarray

--- 53_maximum_subarray.cpp
+++ 53_maximum_subarray.cpp
@@ -22,7 +22,45 @@ int main() {
     cin.tie(nullptr);
     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     Solution solution;
-    int maxSum = solution.maxSubArray(nums);
-    cout << maxSum << '\n';
+    int result = solve.maxSubArray(nums);
+    cout << result << '\n';
     return 0;
 }
+
+/*
+    ------------------
+    Problem Statement:
+    ------------------
+    Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+    ----------
+    Example 1:
+    ----------
+
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+    ----------
+    Example 2:
+    ----------
+
+    Input: nums = [1]
+    Output: 1
+    Explanation: The subarray [1] has the largest sum 1.
+
+    ----------
+    Example 3:
+    ----------
+
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    
+    ------------
+    Constraints:
+    ------------
+
+    1 <= nums.length <= 10^5
+    -10^4 <= nums[i] <= 10^4
+*/
