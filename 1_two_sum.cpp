979fc0832a9941cdd657a3f0125a9153a4309edd
Eklas Mohin <mohin73k@gmail.com>
2026-03-06 12:54:35 +0600

    LeetCode 1. Two Sum

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -33,10 +33,10 @@ int main() {
     Problem Statement:
     ------------------
 
-    Given an array of integers nums and an integer target,
-    return indices of the two numbers such that they add up to
-    target. You may assume that each input would have exactly
-    one solution, and you may not use the same element twice.
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
     You can return the answer in any order.
 
     ----------
