0588d880814e052f3336b1b3961345ebecdeb6c3
Eklas Mohin <mohin73k@gmail.com>
2026-03-07 05:11:36 +0600

    LeetCode 58. Length of Last Word

--- 58_length_of_last_word.cpp
+++ 58_length_of_last_word.cpp
@@ -22,8 +22,50 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     string str = "   fly me   to   the moon  ";
-    Solution solution;
-    int lastWordLength = solution.lengthOfLastWord(str);
-    cout << lastWordLength << '\n';
+    Solution solve;
+    int result = solve.lengthOfLastWord(str);
+    cout << result << '\n';
     return 0;
 }
+
+/*
+    ------------------
+    Problem Statement:
+    ------------------
+
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+    A word is a maximal substring consisting of non-space characters only.
+
+    ----------
+    Example 1:
+    ----------
+
+    Input: s = "Hello World"
+    Output: 5
+    Explanation: The last word is "World" with length 5.
+
+    ----------
+    Example 2:
+    ----------
+
+    Input: s = "   fly me   to   the moon  "
+    Output: 4
+    Explanation: The last word is "moon" with length 4.
+
+    ----------
+    Example 3:
+    ----------
+
+    Input: s = "luffy is still joyboy"
+    Output: 6
+    Explanation: The last word is "joyboy" with length 6.
+    
+    ------------
+    Constraints:
+    ------------
+
+    1 <= s.length <= 10^4
+    s consists of only English letters and spaces ' '.
+    There will be at least one word in s.
+*/
