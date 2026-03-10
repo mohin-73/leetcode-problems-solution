ed92164a6fc08ce6eec702fc612c3a9b8b020b6f
Eklas Mohin <mohin73k@gmail.com>
2026-03-10 22:39:20 +0600

    LeetCode 1390. Four Divisors

--- 1390_four_divisors.cpp
+++ 1390_four_divisors.cpp
@@ -6,23 +6,24 @@ class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
         int divisorSum = 0;
-        for (int num : nums) {
-            if (int(sqrt(num)) * int(sqrt(num)) == num) {
+        for (int i = 0; i < nums.size(); ++i) {
+            int sqr = sqrt(nums[i]);
+            if (sqr * sqr == nums[i]) {
                 continue;
             }
-            int t = num, cnt = 0, a, b;
-            for (int i = 2; i * i <= t; ++i) {
-                if (t % i == 0) {
+            int temp = nums[i], cnt = 0, a, b;
+            for (int j = 2; j * j <= temp; ++j) {
+                if (temp % j == 0) {
                     ++cnt;
-                    a = i;
-                    b = t / i;
+                    a = j;
+                    b = temp / j;
                 }
                 if (cnt > 1) {
                     break;
                 }
             }
             if (cnt == 1) {
-                divisorSum += (1 + num + a + b);
+                divisorSum += (1 + nums[i] + a + b);
             }
         }
         return divisorSum;
@@ -38,3 +39,44 @@ int main() {
     cout << result << '\n';
     return 0;
 }
+
+/*
+    ------------------
+    Problem Statement:
+    ------------------
+
+    Given an integer array nums, return the sum of divisors of the integers in that array that have exactly four divisors. If there is no such integer in the array, return 0.
+
+    ----------
+    Example 1:
+    ----------
+
+    Input: nums = [21,4,7]
+    Output: 32
+    Explanation: 
+    21 has 4 divisors: 1, 3, 7, 21
+    4 has 3 divisors: 1, 2, 4
+    7 has 2 divisors: 1, 7
+    The answer is the sum of divisors of 21 only.
+
+    ----------
+    Example 2:
+    ----------
+
+    Input: nums = [21,21]
+    Output: 64
+
+    ----------
+    Example 3:
+    ----------
+
+    Input: nums = [1,2,3,4,5]
+    Output: 0
+
+    ------------
+    Constraints:
+    ------------
+
+    1 <= nums.length <= 10^4
+    1 <= nums[i] <= 10^5
+*/
