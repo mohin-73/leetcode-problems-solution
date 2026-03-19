3a201037ffcbd5f83fcf80d614073952b991b2e4
Eklas Mohin <mohin73k@gmail.com>
2026-03-19 21:43:42 +0600

    LeetCode 1. Two Sum

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -46,7 +46,7 @@ int main() {
 
     Input: nums = [2,7,11,15], target = 9
     Output: [0,1]
-    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Explanation: Because nums[0] + nums[1] == 9, we return [0,1].
 
     ----------
     Example 2:
